cfce7e1a23adb89aa9ea73a3073f7867159d3872
mudasir687abbas <157064288+mudasir687abbas@users.noreply.github.com>
2026-01-15 16:39:18 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ import mysql from "mysql2/promise";
 let db;
 try{
  db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'web_portal',
+      host: "sql12.freesqldatabase.com",
+      user: "sql12814377",
+      password: "lvNrIK6F93",
+      database: "sql12814377",
+      port: 3306,
     });
   }catch(err){
     console.log("No Database");
